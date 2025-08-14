866ebbbccff3491099f9a788273f2ef32366fdb7
wahahahaya <a870128rlen@gmail.com>
2025-08-14 09:38:44 +0800

    Refactor download script to remove checkpoints and adjust save directory

--- scripts/download/download.sh
+++ scripts/download/download.sh
@@ -3,8 +3,7 @@
 # download the up-to-date benchmarks and checkpoints
 # provided by OpenOOD v1.5
 python ./scripts/download/download.py \
-    --contents 'datasets' 'checkpoints' \
+    --contents 'datasets' \
     --datasets svhn tin \
-    --checkpoints 'ood_v1.5' \
-    --save_dir './data' './results' \
+    --save_dir './data' \
     --dataset_mode 'benchmark'
